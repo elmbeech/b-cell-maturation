3584f693d1d16cb976dddb1dabadb270adecad2d
nonwibb <nonwibb@gmail.com>
2023-04-05 08:16:09 -0400

    make variable for mutation chance

--- PhysiCell/custom_modules/custom.cpp
+++ PhysiCell/custom_modules/custom.cpp
@@ -381,10 +381,11 @@ void bfollicular_cell_phenotype( Cell* pCell, Phenotype& phenotype , double dt )
         printSequence( antibodySequence.value, "Antibody: ");
 
 	// mutate inherited antibody sequnece, but only by cell dividion and only one offspring cell!
+    double mutationChance = 0.5;
     // If cell divided less than 6 minutes ago
     if (phenotype.cycle.data.elapsed_time_in_phase < 6.0f){
         // 50% chance to mutate sequence
-        std::vector<double> mutateProbability = {0.5, 0.5};
+        std::vector<double> mutateProbability = {1-mutationChance, mutationChance};
         if(choose_event(mutateProbability)) {
             mutateSequence( antibodySequence.value, MUTATION );
             pCell->custom_data.vector_variables[antibodyIndex].value = antibodySequence.value;
